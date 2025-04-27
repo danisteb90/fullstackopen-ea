6565d94a4f41a6051eaccd622afb50a72d5b93fc
Esteban <esteban.alban9003@gmail.com>
2025-04-27 18:25:57 -0500

    Initialize phonebook application with React and Vite
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to the public directory.
    - Implemented main application component (App.jsx) with functionality to add and display names in a phonebook.

---
+++ part2/part2_phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const nameObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
+  const handleInput = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInput} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        <ul>
+          {persons.map((person) => (
+            <li key={person.name}>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
